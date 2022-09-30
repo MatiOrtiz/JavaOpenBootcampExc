afd73fbb9838ff8c54041e936b18e6fe8031700e
MatiOrtiz <matiasnortiz.dev@gmail.com>
2022-09-30 00:10:23 -0300

    exc uploaded

--- Exc1/Main.java
+++ Exc1/Main.java
@@ -3,9 +3,29 @@ public class Main {
         int resultado;
         resultado = suma(2, 24, 4);
         System.out.println(resultado);
+
+        Coche miCoche = new Coche();
+        miCoche.incrementar(1);
+        System.out.println("La cantidad de puertas del auto es: " + miCoche.obtenerPuertas());
     }
 
     public static int suma(int a, int b, int c) {
         return a + b + c;
+    }
+}
+
+class Coche {
+    private int puertas;
+
+    public Coche() {
+        puertas = 0;
+    }
+
+    public void incrementar(int p) {
+        puertas += p;
+    }
+
+    public int obtenerPuertas() {
+        return puertas;
     }
 }
